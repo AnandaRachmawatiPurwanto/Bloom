a514069d068ff947e06d0d5c75dcc9b8f72cb0d5
Ananda Rachmawati Purwanto <anandarachmawati287@gmail.com>
2026-06-29 09:56:38 +0700

    Initial Commit

---
+++ Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by Ananda Rachmawati Purwanto on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bloom
+//
+//  Created by Ananda Rachmawati Purwanto on 29/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
